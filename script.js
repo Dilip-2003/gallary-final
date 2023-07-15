f793a69e68b4444e8b8b19170d1f3e26bfe8328f
Dilip-2003 <darkerdip@gmail.com>
2023-07-16 01:02:55 +0530

    changes4

---
+++ script.js
@@ -0,0 +1,37 @@
+// Get all gallery items
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+// Get the preview container and image
+const previewContainer = document.querySelector('.preview-container');
+const previewImage = document.querySelector('.preview-image');
+
+// Loop through each gallery item and attach an event listener
+galleryItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Get the source of the clicked image
+    const src = item.querySelector('img').getAttribute('src');
+
+    // Set the source of the preview image
+    previewImage.setAttribute('src', src);
+
+    // Show the preview container
+    previewContainer.style.display = 'flex';
+    previewImage.style.display ='block';
+   
+  });
+});
+
+// Add event listener to close the preview on click
+previewContainer.addEventListener('click', () => {
+  previewContainer.style.display = 'none';
+});
+
+// function zoomInOut(img) {
+//     if (img.style.cursor === 'zoom-in') {
+//       img.style.cursor = 'zoom-out';
+//     } else {
+//       img.style.cursor = 'zoom-in';
+//     }
+//     // img.style.cursor = 'zoom-out';
+//   }
+  
